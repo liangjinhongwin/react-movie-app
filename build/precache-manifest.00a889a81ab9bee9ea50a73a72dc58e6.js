cd6cb09d95588b12285b4d7e4a2134ac69b104e1
Jinhong Liang <liangjinhongwin@hotmail.com>
2019-12-26 15:56:04 -0800

    rebuild

---
+++ build/precache-manifest.00a889a81ab9bee9ea50a73a72dc58e6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51e137eaf50d039c7fd4dc535ef6ab9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "480fd84965e0d5de9b4c",
+    "url": "/static/css/main.16a74314.chunk.css"
+  },
+  {
+    "revision": "6970b3f9e6469f1ebeb4",
+    "url": "/static/js/2.48b178ac.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.48b178ac.chunk.js.LICENSE"
+  },
+  {
+    "revision": "480fd84965e0d5de9b4c",
+    "url": "/static/js/main.7f7ae306.chunk.js"
+  },
+  {
+    "revision": "cac52f14744f1a251964",
+    "url": "/static/js/runtime-main.5f1d4838.js"
+  },
+  {
+    "revision": "19b5f497446fa0bdd75fd517ac40f583",
+    "url": "/static/media/movie-bg.19b5f497.png"
+  },
+  {
+    "revision": "dc1817ac610e1ea40949e01b8e8ca635",
+    "url": "/static/media/not-found.dc1817ac.png"
+  }
+]);
